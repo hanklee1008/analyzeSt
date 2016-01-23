ddf61d1b717c3b3868cbc7250b32657937d1cb06
ocean1008 <ocean1008@gmail.com>
2016-01-23 11:56:20 +0800

    add comment
    test github push

--- analyzeStockData.java
+++ analyzeStockData.java
@@ -23,7 +23,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
+//test git push
 public class analyzeStockData {
 public String getStockCapital(String stocknum)
 {
