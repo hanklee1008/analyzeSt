c3a03d3d2c5a9ee9db53838b97cdc8bd5791b57c
hanklee <ocean1008@gmail.com>
2016-06-04 18:09:37 +0800

    update
    update

--- analyzeStrategy.java
+++ analyzeStrategy.java
@@ -17,7 +17,7 @@ public class analyzeStrategy {
 			SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");			
 			System.out.println("\ncompute start:"+sdFormat.format(new Date()));
 			
-			String filepath=drive+"software/sdata/15foranalyze/";
+			String filepath=drive+"software/sdata/15temp/";
 			ArrayList<String[]> allTimePoint=new ArrayList<String[]>();		
 			bullStrategy1 st1=new bullStrategy1();
 			Workbook workbook;
@@ -37,7 +37,7 @@ public class analyzeStrategy {
 				
 			}			
 			
-			s1.fillInData(allTimePoint,new File(drive+"software/sdata/c4.xls"),20040301,0);
+			s1.fillInData(allTimePoint,new File(drive+"software/sdata/t8.xls"),20040301,0);
 			
 			System.out.println("\ncompute end:"+sdFormat.format(new Date()));
 
--- bullStrategy1.java
+++ bullStrategy1.java
@@ -76,23 +76,25 @@ public class bullStrategy1 {
 						{
 							isComputeReturn=1;									
 							enterPoint=entrypoint[3];									
-							currentHigh=entrypoint[3];
+							currentHigh=entrypoint[1];
 							currentLow=entrypoint[3];
 							buytime=s.getCell(0,temp).getContents();
 							weeklyrate=(entrypoint[3]-content.get(content.size()-1)[3])/content.get(content.size()-1)[3]*100;
 							lead=(entrypoint[1]-entrypoint[3])/entrypoint[3]*100;
 							quantity=entrypoint[6];
 							
-							/*{
+							double testpoint;
+							{
 								//highWeeklyrate=(entrypoint[1]-content.get(content.size()-1)[3])/content.get(content.size()-1)[3]*100;
 								
 								//if(weeklyrate>=6&&highWeeklyrate>=divideWeeklyrate)
-								{													
+								{		
+									testpoint=computeEnterPoint(content,entrypoint);
 									//enterPoint=computeEnterPoint(content,entrypoint);
-									currentLow=enterPoint;
-									currentHigh=enterPoint;
+									//currentLow=enterPoint;
+									//currentHigh=entrypoint[1];;
 								}
-							}*/
+							}
 							
 							as.add(buyday[kstate[2]]);
 							buytimeAll.add(buytime);
@@ -106,7 +108,7 @@ public class bullStrategy1 {
 							dd[6]=weeklyrate;
 							mdata.add(dd);
 
-							tempdata=new String[]{stockname,buyday[kstate[2]],"",""+test,"","",quantity+"",df.format(weeklyrate),df.format(lead),"","","","","","","","","","1"};
+							tempdata=new String[]{stockname,buyday[kstate[2]],"",""+(currentHigh-testpoint)/content.get(content.size()-1)[3],""+test,"",quantity+"",df.format(weeklyrate),df.format(lead),"","","","","","","","","","1"};
 
 							returnV[0]=currentHigh;
 							returnV[1]=currentLow;
@@ -216,24 +218,25 @@ public class bullStrategy1 {
 		
 		computeDailyK(shfile,s.getCell(0,temp).getContents(),combineK,contemp,buyday,currentK);
 		
-		/*ArrayList<double[]> ttt=new ArrayList<double[]>();
+		ArrayList<double[]> ttt=new ArrayList<double[]>();
 		String[] buyday1=new String[7];
-		computeDailyKKK(shfile,s.getCell(0,temp).getContents(),ttt,contemp,buyday1);*/
+		computeDailyKKK(shfile,s.getCell(0,temp).getContents(),ttt,contemp,buyday1);
 
 		if(combineK.size()!=0)
 			do{//System.out.println(s.getCell(0,temp).getContents()+" "+day+" "+tt.get(day)[3]);		
-				/*if(conditionAnalyzeKKK(content,ttt.get(day),ttt.get(day)[3]))
+				if(conditionAnalyzeKKK(content,ttt.get(day),ttt.get(day)[3]))
 				{
 					double enterPoint=computeEnterPointKKK(content,ttt.get(day));
 					
-					if(enterPoint<ttt.get(day)[3])
+					if(((ttt.get(day)[1]-enterPoint)/content.get(content.size()-1)[3])>=0.02)
 					System.out.println("1:"+buyday1[day]+" "+(enterPoint)+" "+ttt.get(day)[3]);
-				}*/
+				}
 				if (conditionAnalyze(content,combineK.get(day),combineK.get(day)[3],currentK.get(day)))
 				{
 					System.arraycopy(combineK.get(day), 0, entrypoint, 0, entrypoint.length);
+					System.out.println("2:"+buyday[day]);
 					/*kstate[1]=1;
-					//System.out.println("2:"+buyday[day]);
+					
 					for (int i=content.size()-1;i>=1;i--)
 					{
 						if (content.get(i)[4]>content.get(i-1)[4])
@@ -253,7 +256,7 @@ public class bullStrategy1 {
 	}
 	private boolean conditionAnalyze(ArrayList<double[]> base,double[] compare,double enterPoint,double[] currentk)
 	{	
-		test=(currentk[3]-currentk[0])/currentk[0];
+		test=(currentk[1]-currentk[0])/currentk[0];
 		
 		int weeklyRateType;
 		//quantityType=isOverQuantity(base,compare);
@@ -330,22 +333,26 @@ public class bullStrategy1 {
 	}
 	public boolean kType(int quantityType,ArrayList<double[]> base,double[] compare,double enterPoint)
 	{
-		if ((test=isRedK(quantityType,base,compare,enterPoint))!=0)
+		if ((isRedK(quantityType,base,compare,enterPoint))!=0)
 			return true;		
 
 		return false;	
 	}
 	public int isRedK(int quantityType,ArrayList<double[]> base,double[] compare,double enterPoint)
 	{//System.out.print("\nisRedK");
-				
-		if((compare[1] - base.get(base.size() - 1)[3]) / base.get(base.size() - 1)[3] >=0.09&&compare[1]==compare[3])
+		
+		if ((enterPoint-compare[0])/compare[0]*100>=6)//次跋办>=5%
+		{
+			return 2;
+		}	
+		/*if((compare[1] - base.get(base.size() - 1)[3]) / base.get(base.size() - 1)[3] >=0.09&&compare[1]==compare[3])
 			return 9;
 
-		/*if (base.get(base.size() - 1)[1]== base.get(base.size() - 1)[3]&&(enterPoint - base.get(base.size() - 1)[3]) / base.get(base.size() - 1)[3] >=0.05)
+		if (base.get(base.size() - 1)[1]== base.get(base.size() - 1)[3]&&(enterPoint - base.get(base.size() - 1)[3]) / base.get(base.size() - 1)[3] >=0.05)
 		{
 			return 1;
 		}
-		else*/
+		else
 		{
 			if ((enterPoint-compare[0])/compare[0]*100>=5)//次跋办>=5%
 			{
@@ -353,7 +360,7 @@ public class bullStrategy1 {
 			}	
 			else
 			{
-				/*if((enterPoint-compare[2])/compare[2]*100>=7)//眖翴衡>=7%
+				if((enterPoint-compare[2])/compare[2]*100>=7)//眖翴衡>=7%
 				{
 					if ((enterPoint - compare[0]) / compare[0] > 0.02)// 2%次
 					{
@@ -372,9 +379,9 @@ public class bullStrategy1 {
 				else
 				{
 					return 0;
-				}*/
+				}
 			}	
-		}	
+		}	*/
 
 		return 0;
 	}
@@ -602,6 +609,7 @@ public class bullStrategy1 {
 	}
 	public boolean endComputeReturn(Sheet s,String name,String buytime,double predictpoint,double[] returnV,int day)
 	{			
+		final double stop_percent=0.13;
 		int row=0;	
 		double[] contemp=new double[5];
 		double enterPoint=predictpoint;									
@@ -645,7 +653,7 @@ public class bullStrategy1 {
 						returnV[0]=contemp[1];	
 					}
 
-					if ((enterPoint-contemp[2])/enterPoint>0.13)//┰禬筁13%
+					if ((enterPoint-contemp[2])/enterPoint>stop_percent)//┰禬筁13%
 					{
 						return true;
 					}
@@ -675,7 +683,7 @@ public class bullStrategy1 {
 						}
 					}
 
-					if ((enterPoint-contemp[2])/enterPoint>0.13)//┰禬筁13%
+					if ((enterPoint-contemp[2])/enterPoint>stop_percent)//┰禬筁13%
 					{
 						return true;
 					}
@@ -785,12 +793,13 @@ public class bullStrategy1 {
 	{
 		double keypoint;
 		
-		keypoint=1.03*base.get(base.size()-1)[3];
+		keypoint=compare[0]*1.06;
+		//keypoint=base.get(base.size()-1)[3];
 
 		while(keypoint<compare[3])
 		{
 			if(isTurnQuarterLine(base,compare,keypoint))//﹗絬		
-				if(isTurnMonthLine(base,compare,keypoint))//る絬		
+				if(isTurnMonthLine(base,compare,keypoint))//る絬	
 					if (kType(0,base,compare,keypoint))
 					{
 						break;
@@ -799,10 +808,10 @@ public class bullStrategy1 {
 
 		}
 
-		keypoint*=1.03;
+		//keypoint*=1.03;
 
-		//if(keypoint>compare[1])
-		//	keypoint=compare[1];
+		if(keypoint>compare[3])
+			keypoint=compare[3];
 
 		return keypoint;
 	}
@@ -966,10 +975,10 @@ public class bullStrategy1 {
 					
 					ktype[6]=Double.parseDouble(s.getCell(9,row+day).getContents());
 				}
-				if (Double.parseDouble(s.getCell(2,row+day).getContents())>=Double.parseDouble(s.getCell(1,row+day).getContents())*1.03)
+				/*if (Double.parseDouble(s.getCell(2,row+day).getContents())>=Double.parseDouble(s.getCell(1,row+day).getContents())*1.03)
 					ktype[3]=Double.parseDouble(s.getCell(2,row+day).getContents());
-				else
-					ktype[3]=Double.parseDouble(s.getCell(4,row+day).getContents());
+				else*/
+					ktype[3]=Double.parseDouble(s.getCell(2,row+day).getContents());
 
 				ktype[4]=(ktype[3]-base[3])/4+base[4];
 				ktype[5]=(ktype[3]-base[3])/13+base[5];
